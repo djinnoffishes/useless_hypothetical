f2da590547c7a18b199f75fecf86a8269179125b
Mitch <wabbitsnot@gmail.com>
2013-07-30 11:32:48 -0700

    fixed migration

--- db/migrate/20130726165246_create_hypotheticals.rb
+++ db/migrate/20130726165246_create_hypotheticals.rb
@@ -3,8 +3,6 @@ class CreateHypotheticals < ActiveRecord::Migration
     create_table :hypotheticals do |t|
       t.string :hypo1
       t.string :hypo2
-      t.integer :hypo1_votes
-      t.integer :hypo2_votes
 
       t.timestamps
     end
